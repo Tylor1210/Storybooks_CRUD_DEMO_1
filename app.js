e154da647e4d21c27750e1d0e4a2403d3254db27
Tylor C <100950093+Tylor1210@users.noreply.github.com>
2022-08-09 03:36:39 -0400

    Second Push

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const connectDB = require('./config/db')
+const MongoStore = require('connect-mongo')
 
 //Load config
 dotenv.config({ path: './config/config.env'})
@@ -18,14 +20,41 @@ connectDB()
 
 const app = express()
 
-//Logging
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+//Body Parser
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+  )
+  
+//DETERMINE LEVEL OF LOGGING
+  if(process.env.NODE_ENV === 'development') {
+      app.use(morgan('dev'))
 }
+  
+//Handlebars HELPERS
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+  
 
 //Handlebars
 //!Add the word .engine after exphbs
 app.engine('.hbs', exphbs.engine({
+    helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select
+    },
     defaultLayout: 'main',
     extname: '.hbs'
     })
@@ -38,6 +67,9 @@ app.use(
         secret:'keyboard cat',
         resave: false,
         saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI
+        })
     })
 )
 
@@ -46,12 +78,19 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Set Global variable
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
+
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 8500
 
